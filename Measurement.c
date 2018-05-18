050d66488c7e781c4c5e6045fd40b6cead7c52fe
Nicholas Gay <frezzer_nick@yahoo.com.sg>
2018-05-18 15:05:51 +0200

    Changed main to a function

---
+++ Measurement.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include <sys/time.h>
+
+#include "Test.c"
+#include "File.c"
+
+#define packetsize 800
+
+int Measurement() {
+
+    int i;
+
+    char Msg1[packetsize] = "49 49 Undefeated";
+    char Msg2[packetsize] = "Playing Football The Arsenal Way";
+    char Mail1[packetsize];
+    char Mail2[packetsize];
+
+    int sock;       //Handler for socket
+    struct timeval start, end;      //For Gettime
+    double t;       //Dispersion Time
+    double Rate;       //Bottleneck Rate
+
+    unsigned int Interations = 200;       //Number of Interations
+    
+    sock = connectsock("129.187.223.200", "2000", "udp");       //Creating Socket
+
+    Overwritefile();        //Erasing Past Results
+    if(Test(sock)==0){
+        for(i=1;i<=Interations;i++){
+
+            send(sock,Msg1,packetsize,0);       //Sending first packet
+            send(sock,Msg2,packetsize,0);       //Sending Second packet
+            if(recv(sock,Mail1,packetsize,0)>0){
+                gettimeofday(&start,NULL);      //Time starts on receiving first packet
+            }
+            if(recv(sock,Mail2,packetsize,0)>0){
+                gettimeofday(&end,NULL);        //Time ends on receiving second packet
+            }
+            
+            t = end.tv_usec - start.tv_usec;      //Calculating Dispersion Time
+            Rate = packetsize/t;                    //Calculating Rate
+
+            WriteFile(Rate);        //Writing to File
+
+            printf("Rate is :%f\n",Rate);        //Printing
+        }
+    }
+}
