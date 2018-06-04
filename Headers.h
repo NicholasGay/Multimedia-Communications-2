97029535fdbac8599e802cc02b4769744f65271d
Nicholas Gay <frezzer_nick@yahoo.com.sg>
2018-06-04 20:40:03 +0200

    Fixed while loop

--- Headers.h
+++ Headers.h
@@ -1,4 +1,8 @@
 #include <stdio.h>
 #include "connectsock.c"
 #include "Test.c"
+
 int sock;
+char command[1500] = "TEXT";
+char msg[1500];
+char end[1500] = "END";
--- main.c
+++ main.c
@@ -4,18 +4,13 @@ void main(){
    sock = connectsock("129.187.223.200", "3000", "udp");
 
   Test(sock);       //Testing program
-
-  char command[1500] = "TEXT";
-  char msg[1500];
-  char end[1500] = "END";
   //int packet = 1;
   send(sock,command,1500,0);
 
-  while(msg != end){
-    //printf("Packet :%d\n",packet);
+  while(strcmp(msg,end) != 0){
     recv(sock, msg,1500,0);
     printf("%s\n",msg);
-    //packet++;
+  
   }
   
   
