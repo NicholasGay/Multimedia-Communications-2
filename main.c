3c47918f0a27a8e39db969d30c3894bdb0a15e2e
Nicholas Gay <frezzer_nick@yahoo.com.sg>
2018-06-04 19:51:40 +0200

    NOT DONE

--- main.c
+++ main.c
@@ -3,7 +3,20 @@
 void main(){
    sock = connectsock("129.187.223.200", "3000", "udp");
 
-   if(Test(sock) == 0){
-       printf("yay\n");
-   }
+  Test(sock);       //Testing program
+
+  char command[1500] = "TEXT";
+  char msg[1500];
+  char end[1500] = "END";
+  //int packet = 1;
+  send(sock,command,1500,0);
+
+  while(msg != end){
+    //printf("Packet :%d\n",packet);
+    recv(sock, msg,1500,0);
+    printf("%s\n",msg);
+    //packet++;
+  }
+  
+  
 }
