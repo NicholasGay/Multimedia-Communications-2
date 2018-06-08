e784bb5197f106247288318b95fb19195845aeb7
Nicholas Gay <frezzer_nick@yahoo.com.sg>
2018-06-08 23:06:22 +0200

    clean

--- main.c
+++ main.c
@@ -20,3 +20,25 @@ void main(){
 
   printf("%s\n",msg);
 }
+#include "Headers.h"
+
+void main(){
+
+  Overwritefile(); 
+  sock = connectsock("129.187.223.200", "3000", "udp");
+
+  Test(sock);       //Testing program
+  //int packet = 1;
+  send(sock,command,1500,0);
+
+  while(1){
+    recv(sock, msg,1500,0);
+    if(strcmp(msg,end) == 0){
+      break;
+    }
+    seq = (msg[2]<<8)+msg[3];
+    printf("%d\n",seq);
+  }
+
+  printf("%s\n",msg);
+}
