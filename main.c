3d78bc81fb72ac1a3136abe586f8cf0a5ceea4eb
Nicholas Gay <frezzer_nick@yahoo.com.sg>
2018-06-09 10:01:10 +0200

    Working

--- main.c
+++ main.c
@@ -25,24 +25,35 @@ void main(){
     index++;   
   }
 
-  
-  for(int i=0;i<index;i++){
-    if(input[i].seq = i){
-      fwrite(input[i].payload,1,input[i].payload_size,fp);
-      
-    }
-    else{
-      int j = i;
-      while(input[j].seq != i){
-        j++;
-        if(input[j].seq == i){
-          fwrite(input[j].payload,1,input[j].payload_size,fp);
-          i++;
-          break;
-        }
+  struct data temp;
+  int swapped;
+  while(1){
+    swapped = 0;
+    for(int i = 0; i<index-1; i++){
+
+      if(input[i].seq > input[i+1].seq){
+          temp = input[i];
+          input[i] = input[i+1];
+          input[i+1] = temp;
+          swapped = 1;
       }
     }
+
+    if(swapped == 0){
+      break;
+    }
   }
+      
+  
+
+ for(int i = 0; i<index; i++){
+   if(input[i].seq != i){
+     printf("Wrong\n");
+   }
+   else{
+     fwrite(input[i].payload,1,input[i].payload_size,fp);
+   }
+ }
  fclose(fp);
  printf("%s\n",input[1].payload);
  
