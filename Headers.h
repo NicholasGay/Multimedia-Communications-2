3976aee4d432fbbe9980e941f5c654dad0f594f7
Nicholas Gay <frezzer_nick@yahoo.com.sg>
2018-06-09 10:14:09 +0200

    Done

--- Headers.h
+++ Headers.h
@@ -4,13 +4,6 @@
 #include "File.c"
 
 int sock;
-char command[1500] = "MUSIC 03689338";
-
-uint16_t seq;
-uint8_t msg[1500];
-uint8_t end[1500] = "END";
-
-struct data{
-    uint16_t seq;
-    uint8_t payload[];
-};
+char command[1500] = "TEXT";
+char msg[1500];
+char end[1500] = "END";
